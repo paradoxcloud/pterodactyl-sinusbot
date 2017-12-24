ed47297b4e64d827b236cf71a19600f57cbb299c
Arajdon <pawlus.patrick@gmail.com>
2017-12-24 19:50:25 +0100

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+sleep 2
+
+cd /home/container
+
+# Make internal Docker IP address available to processes.
+export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
+
+# Update Source Server
+
+
+# Replace Startup Variables
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+${MODIFIED_STARTUP}
